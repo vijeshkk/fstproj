d1eecc43566121e9b702907f357c461d0baea550
vijesh <vijeshkk@gmail.com>
2017-03-27 23:17:55 +0800

    Initial Commit

---
+++ app/products/product-list.component.js
@@ -0,0 +1,71 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var ProductListComponent = (function () {
+    function ProductListComponent() {
+        this.pageTitle = "Product List";
+        this.imageWidth = 50;
+        this.imageMargin = 2;
+        this.showImage = false;
+        this.products = [
+            {
+                "productId": 8,
+                "productName": "Saw",
+                "productCode": "TBX-0022",
+                "releaseDate": "May 15, 2016",
+                "description": "15-inch steel blade hand saw",
+                "price": 11.55,
+                "starRating": 3.7,
+                "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+            },
+            {
+                "productId": 10,
+                "productName": "Video Game Controller",
+                "productCode": "GMG-0042",
+                "releaseDate": "October 15, 2015",
+                "description": "Standard two-button video game controller",
+                "price": 35.95,
+                "starRating": 4.6,
+                "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+            },
+            {
+                "productId": 10,
+                "productName": "Video Game Controller",
+                "productCode": "GMG-0042",
+                "releaseDate": "October 15, 2015",
+                "description": "Standard two-button video game controller",
+                "price": 35.95,
+                "starRating": 4.6,
+                "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+            },
+            {
+                "productId": 10,
+                "productName": "Video Game Controller",
+                "productCode": "GMG-0042",
+                "releaseDate": "October 15, 2015",
+                "description": "Standard two-button video game controller",
+                "price": 35.95,
+                "starRating": 4.6,
+                "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+            }
+        ];
+    }
+    ProductListComponent.prototype.toggleImage = function () {
+        this.showImage = !this.showImage;
+    };
+    return ProductListComponent;
+}());
+ProductListComponent = __decorate([
+    core_1.Component({
+        selector: 'pm-products',
+        templateUrl: 'app/products/product-list.component.html'
+    })
+], ProductListComponent);
+exports.ProductListComponent = ProductListComponent;
+//# sourceMappingURL=product-list.component.js.map
---
+++ app/products/product-list.component.ts
@@ -0,0 +1,58 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: 'app/products/product-list.component.html'
+})
+
+export class ProductListComponent{
+    pageTitle: String = "Product List";
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    products: any[] = [
+            {
+        "productId": 8,
+        "productName": "Saw",
+        "productCode": "TBX-0022",
+        "releaseDate": "May 15, 2016",
+        "description": "15-inch steel blade hand saw",
+        "price": 11.55,
+        "starRating": 3.7,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+    },
+    {
+        "productId": 10,
+        "productName": "Video Game Controller",
+        "productCode": "GMG-0042",
+        "releaseDate": "October 15, 2015",
+        "description": "Standard two-button video game controller",
+        "price": 35.95,
+        "starRating": 4.6,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+    },
+    {
+        "productId": 10,
+        "productName": "Video Game Controller",
+        "productCode": "GMG-0042",
+        "releaseDate": "October 15, 2015",
+        "description": "Standard two-button video game controller",
+        "price": 35.95,
+        "starRating": 4.6,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+    },
+    {
+        "productId": 10,
+        "productName": "Video Game Controller",
+        "productCode": "GMG-0042",
+        "releaseDate": "October 15, 2015",
+        "description": "Standard two-button video game controller",
+        "price": 35.95,
+        "starRating": 4.6,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+    }
+    ];
+    toggleImage():void{
+        this.showImage = !this.showImage;
+    }
+}
